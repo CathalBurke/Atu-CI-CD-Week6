de183a1a75b902034d5c0aedb51eee4b814c56e8
CATHALBURKE-STUDENT <g00413483@atu.ie>
2025-10-17 10:10:20 +0100

    added the passenger service class , find by all returns all passengers and a create passenger that handles duplicate ID's

---
+++ src/main/java/ie/atu/week5/service/passengerService.java
@@ -0,0 +1,35 @@
+package ie.atu.week5.service;
+
+import ie.atu.week5.model.passenger;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class passengerService {
+    private final List<passenger> store = new ArrayList<>();
+
+    public List<passenger> findAll() {
+        return  new ArrayList<>(store);
+    }
+
+    public Optional<passenger> findById(String id){
+        for (passenger p : store){
+            if (p.getPassengerId().equals(id)){
+                return  Optional.of(p);
+
+            }
+        }
+        return Optional.empty();
+    }
+
+    public passenger create(passenger p){
+        if (findById(p.getPassengerId()).isPresent()){
+            throw new IllegalArgumentException("Passenger ID already exists");
+        }
+        store.add(p);
+        return p;
+    }
+
+}
